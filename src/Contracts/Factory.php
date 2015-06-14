9011f3f4558986e34ece7202bc18df5d5b3cfedd
Erol Uysal <eutkuuysal@gmail.com>
2015-06-15 01:47:28 +0300

    fix contract.

--- src/Contracts/Factory.php
+++ src/Contracts/Factory.php
@@ -4,4 +4,29 @@ namespace QweB\Facebook\Contracts;
 
 interface Factory
 {
+    /**
+     * Create a new Facebook request with redirect session.
+     *
+     * @param  string  $method
+     * @param  string  $path
+     * @param  array   $parameters
+     * @return \Facebook\FacebookResponse
+     */
+    public function createRequest($method, $path, array $parameters = []);
+
+    /**
+     * Stores CSRF state and returns a URL to which the user should be sent to
+     * in order to continue the login process with Facebook.
+     *
+     * @param  array   $scopes
+     * @return string
+     */
+    public function getLoginUrl(array $scopes = []);
+
+    /**
+     * Get the access token from redirect of current session.
+     *
+     * @return \Facebook\Entities\AccessToken
+     */
+    public function getAccessToken();
 }
